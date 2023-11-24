a31c1b898c33c2567b6ef3fb40ad851f26f319da
david <h158201@stud.u-szeged.hu>
2023-11-24 13:29:21 +0100

    # Create basic spring boot core

---
+++ NEPTUN/src/test/java/com/ETR/NEPTUN/NeptunApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ETR.NEPTUN;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeptunApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
